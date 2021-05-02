8923ad2e91fbb644a7eee04e40dfa7195ca85496
Kevin Nguyen <nguyenkevin1162@gmail.com>
2021-05-01 19:51:21 -0700

    After user clicks "check out" the name, books, and book total gets sent to Stripe with the API key

---
+++ src/pages/Checkout.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { AmplifyAuthenticator } from "@aws-amplify/ui-react";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+import CheckoutForm from "../components/CheckoutForm";
+
+const Checkout = () => {
+  const stripePromise = loadStripe(
+    "pk_test_51IZLe6D5tjsf8sbaeKETA9ZfxJeWjC2W3Ii5QRlcMzre9dqGVkbC2EjWk0P7CSXdu9cIto6Ng7bs5bgh2AzCUVkt00Be1rPAJE"
+  );
+
+  return (
+    <section className="checkout-wrapper">
+      <AmplifyAuthenticator>
+        <Elements stripe={stripePromise}>
+          <section>
+            <h2>Time to Checkout?</h2>
+            <CheckoutForm />
+          </section>
+        </Elements>
+      </AmplifyAuthenticator>
+    </section>
+  );
+};
+
+export default Checkout;
