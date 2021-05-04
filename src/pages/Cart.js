9e74f5658c7914fbab07c3fa5f61b4688ce463e0
VuongAlexander <49496229+VuongAlexander@users.noreply.github.com>
2021-05-03 22:25:30 -0700

    Changed String on Cart page

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -9,7 +9,7 @@ const Cart = () => {
   const { cart, total, increaseAmount, decreaseAmount } = useContext(CartContext);
 
   if (!cart.length) {
-    return <h3>Empty Cart</h3>
+    return <h3>Cart is Empty</h3>
   }
   return (
     <section className="cart">
